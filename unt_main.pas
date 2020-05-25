e6b0bd6f1b73677d3407ca7f0f3f661372d85c07
jeferssonjeiel <jeferssonjeiel@live.com>
2020-05-25 15:02:02 -0300

    1.0.1

--- unt_main.pas
+++ unt_main.pas
@@ -5,7 +5,7 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls,
-  FMX.Controls.Presentation, FMX.TabControl;
+  FMX.Controls.Presentation, FMX.TabControl, FMX.ListBox, FMX.Layouts, FMX.Edit;
 
 type
   Tfrm_main = class(TForm)
@@ -16,6 +16,26 @@ type
     TabControl1: TTabControl;
     tbValorHora: TTabItem;
     tbValorServico: TTabItem;
+    ListBox1: TListBox;
+    ListBoxGroupHeader1: TListBoxGroupHeader;
+    ListBoxItem1: TListBoxItem;
+    Edit1: TEdit;
+    ListBoxGroupHeader2: TListBoxGroupHeader;
+    ListBoxItem2: TListBoxItem;
+    Edit2: TEdit;
+    ListBoxGroupHeader3: TListBoxGroupHeader;
+    ListBoxItem3: TListBoxItem;
+    Edit3: TEdit;
+    ListBox2: TListBox;
+    ListBoxGroupHeader4: TListBoxGroupHeader;
+    ListBoxItem4: TListBoxItem;
+    Edit4: TEdit;
+    ListBoxGroupHeader5: TListBoxGroupHeader;
+    ListBoxItem5: TListBoxItem;
+    Edit5: TEdit;
+    ListBoxGroupHeader6: TListBoxGroupHeader;
+    ListBoxItem6: TListBoxItem;
+    Edit6: TEdit;
   private
     { Private declarations }
   public
